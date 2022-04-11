cd84b272f54c29c56b8a78d773e19298f8227dd7
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-04-11 13:56:34 +0200

    Restructure the js code

--- js/controller.js
+++ js/controller.js
@@ -1,33 +1,19 @@
-// Load boards from file or manually
-const easy = [
-	'6------7------5-2------1---362----81--96-----71--9-4-5-2---651---78----345-------',
-	'685329174971485326234761859362574981549618732718293465823946517197852643456137298',
-];
-const medium = [
-	'--9-------4----6-758-31----15--4-36-------4-8----9-------75----3-------1--2--3--',
-	'619472583243985617587316924158247369926531478734698152891754236365829741472163895',
-];
-const hard = [
-	'-1-5-------97-42----5----7-5---3---7-6--2-41---8--5---1-4------2-3-----9-7----8--',
-	'712583694639714258845269173521436987367928415498175326184697532253841769976352841',
-];
+import * as model from './model.js';
+// import boardView from './views/boardView.js';
+// import controlView from './views/controlView.js';
+// import digitsView from './views/digitsView.js';
 
 // Create variables
 var selectedNum;
 var selectedTile;
 var disableSelect;
 
-window.onload = function () {
-	// Run startgame function when button is clicked
-	id('start-btn').addEventListener('click', startGame);
-};
-
 function startGame() {
 	// Choose board difficulty
 	let board;
-	if (id('easy').checked) board = easy[0];
-	else if (id('medium').checked) board = medium[0];
-	else board = hard[0];
+	if (document.getElementById('easy').checked) board = model.easy[0];
+	else if (document.getElementById('medium').checked) board = model.medium[0];
+	else board = model.hard[0];
 	disableSelect = false;
 	// Create board based on difficulty
 	generateBoard(board);
@@ -35,7 +21,7 @@ function startGame() {
 	// ToDo
 
 	// Show number container
-	id('number-container').classList.remove('hidden');
+	document.getElementById('number-container').classList.remove('hidden');
 }
 
 function generateBoard(board) {
@@ -43,7 +29,7 @@ function generateBoard(board) {
 	clearPrevious();
 
 	// Select all tiles
-	let tiles = qsa('.tile--small');
+	let tiles = document.querySelectorAll('.tile--small');
 
 	for (let i = 0; i < tiles.length; i++) {
 		if (board.charAt(i) != '-') {
@@ -102,7 +88,7 @@ function generateBoard(board) {
 
 function clearPrevious() {
 	// Access all of the tiles
-	// let tiles = qsa('.tile--small');
+	// let tiles = document.querySelectorAll('.tile--small');
 	// Remove each tile
 	// for (let i = 0; i < tiles.length; i++) {
 	// 	tiles[i].remove();
@@ -112,23 +98,25 @@ function clearPrevious() {
 	//
 
 	// Deselect any numbers
-	for (let i = 0; i < id('number-container').children.length; i++) {
-		id('number-container').children[i].classList.remove('selected');
+	for (
+		let i = 0;
+		i < document.getElementById('number-container').children.length;
+		i++
+	) {
+		document
+			.getElementById('number-container')
+			.children[i].classList.remove('selected');
 	}
 	// Clear selected variables
 	selectedNum = null;
 	selectedTile = null;
 }
 
-// Helper functions
-function id(id) {
-	return document.getElementById(id);
-}
+const init = function () {
+	window.onload = function () {
+		// Run startgame function when button is clicked
+		document.getElementById('start-btn').addEventListener('click', startGame);
+	};
+};
 
-function qs(selector) {
-	return document.querySelector(selector);
-}
-
-function qsa(selector) {
-	return document.querySelectorAll(selector);
-}
+init();
