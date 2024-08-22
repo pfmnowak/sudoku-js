a413acc5bf456264951e7141e39e46d94a379c97
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2024-08-22 21:26:39 +0200

    Add the footer

---
+++ src/components/SocialLink.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="social">
+    <a :href="link" class="social__link" :title="title">
+      <svg class="social__icon">
+        <use xlink:href="../assets/sprite.svg#icon-github"></use>
+      </svg>
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    link: {
+      required: true,
+      type: String
+    },
+    title: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.social {
+  display: flex;
+  justify-content: center;
+
+  &__link {
+    width: 5rem;
+    height: 5rem;
+    padding: 1rem;
+    background-color: #ddd;
+    border-radius: 50%;
+    margin-right: 1rem;
+
+    &:hover {
+      background-color: #fff;
+    }
+  }
+
+  &__icon {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer>
+    <social-link
+      link="https://github.com/pfmnowak/sudoku-js/"
+      title="GitHub Repo with the code"
+    ></social-link>
+    <div class="footer__info">
+      <div>
+        &copy; 2022 | By
+        <a href="https://pfmnowak.github.io/" class="inline-link" title="Author's page"
+          >Mikołaj Nowak</a
+        >
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import SocialLink from '../SocialLink.vue'
+
+export default {
+  components: { SocialLink }
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  /* //   color: $color-gray-light; */
+  text-align: center;
+  padding: 3rem 0;
+  margin-top: 5rem;
+  /* //   font-size: $default-font-size; */
+}
+</style>
